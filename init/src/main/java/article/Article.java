b68b62829803851147eb6540b32f912f79db9705
Joseph Torralba <>
2021-04-07 22:35:43 -0700

    add custom password length (< 8 chars) error message

--- init/src/main/java/article/Article.java
+++ init/src/main/java/article/Article.java
@@ -3,6 +3,7 @@ package article;
 import java.util.*;
 import com.psddev.cms.db.Content;
 import com.psddev.cms.db.ToolUi;
+import com.psddev.dari.db.State;
 import com.psddev.dari.util.StorageItem;
 import com.psddev.dari.util.StringUtils;
 import com.psddev.cms.db.Directory;
@@ -23,6 +24,7 @@ public class Article extends Content implements Directory.Item  {
     private Date date; // a date widget
     private boolean switchComponent; // a toggle on the page
     @ToolUi.Secret
+    @ToolUi.Note("must be greater than 8 characters")
     private String password; // a hidden text "password" field in the UI
     @ToolUi.AddToTop
     private List<Author> listOfAuthors; // a list of authors
@@ -59,6 +61,19 @@ public class Article extends Content implements Directory.Item  {
     @Override
     public String createPermalink(Site site) {
         return StringUtils.toNormalized(getHeadline());
+    }
+
+    protected void beforeSave() {
+        // check if password is the correct length
+        try {
+            if (password.length() < 8) {
+                throw new Exception();
+            }
+        } catch (final Exception e) {
+            State state = getState();
+            state.addError(state.getField("password"), "Password must be 8 characters or more.");
+            throw new IllegalArgumentException("Resolve the error with the password, then retry Publish.");
+        }
     }
 
 }
